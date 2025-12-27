d835b426af20453a6150c29364f1a7b6f4d177ce
Davis Berlind <davis.berlind@gmail.com>
2025-12-27 02:09:24 -0800

    fixing well log plots

--- well_log/well_log.R
+++ well_log/well_log.R
@@ -6,6 +6,8 @@ library(L2hdchange)
 library(ecp)
 library(mich)
 
+source("../simulations/simulation_functions.R")
+
 # read in well log data
 well_log <- read.csv("./facies_data.csv")
 
@@ -19,6 +21,8 @@ well <- well_log %>%
 # extract changepoints
 true_cp <- which(diff(well$Facies) != 0) + 1
 T <- nrow(well)
+depth = well$Depth
+well$Depth = 1:T
 
 well <- well %>% mutate(Facies = case_when(Facies == 1 ~ "Nonmarine Sandstone",
                                            Facies == 2 ~ "Nonmarine Coarse Siltstone",
@@ -62,31 +66,33 @@ well_pivot %>%
 dev.off()
 
 # fit MICH ####
-fit <- mich(well[,-c(1,2)], L_auto = TRUE, tol = 1e-3, restart = FALSE, verbose = TRUE)
-fit_rev <- mich(well[,-c(1,2)], L_auto = TRUE, tol = 1e-10, restart = FALSE, reverse = TRUE, verbose = TRUE)
+fit <- mich(well[,-c(1,2)], L_auto = TRUE, tol = 1e-10, restart = FALSE, verbose = TRUE)
 
-if (max(fit_rev$elbo) > max(fit$elbo)) fit <- fit_rev
+est_cp <- mich_sets(fit$pi_bar, level = 0.99)$cp
+sets <- mich_sets(fit$pi_bar, level = 0.99)$sets
 
-est_cp <- mich_sets(fit_fast_rev$pi_bar_l, level = 0.99)$cp
-sets <- mich_sets(fit_fast_rev$pi_bar_l, level = 0.99)$sets
+abs(length(true_cp) - length(est_cp))
+fnsle(c(1,true_cp,T), c(1,est_cp,T))
+fpsle(c(1,true_cp,T), c(1,est_cp,T))
 
-# sum(apply(abs(outer(unlist(sets), true_cp, `-`)), 2, min) <= 1)
-# length(est_cp[sapply(sets, function(set) min(apply(abs(outer(set, true_cp, `-`)),1,min))) <= 0])
-# length(est_cp[sapply(sets, function(set) min(apply(abs(outer(set, true_cp, `-`)),1,min))) <= 1])
+sum(apply(abs(outer(unlist(sets), true_cp, `-`)), 1, min) < 1)
+sum(apply(abs(outer(unlist(sets), true_cp, `-`)), 1, min) <= 1)
+length(est_cp[sapply(sets, function(set) min(apply(abs(outer(set, true_cp, `-`)),1,min))) <= 0])
+length(est_cp[sapply(sets, function(set) min(apply(abs(outer(set, true_cp, `-`)),1,min))) <= 1])
 
-png("~/mich_well_log.png", width = 1300, height = (6.5 / 10) * 1300)
+png("./mich_well_log.png", width = 1300, height = (6.5 / 17.5) * 1300)
 well_pivot %>%
   mutate(Value_end = well_pivot2$Value,
          Depth_end = well_pivot2$Depth) %>%
   ggplot() +
-  geom_vline(xintercept = well$Depth[unlist(sets)], color = "lightblue", alpha = 0.6, size = 2.5) +
+  geom_vline(xintercept = unlist(sets), color = "lightblue", alpha = 0.6, size = 2.5) +
   geom_segment(aes(x = Depth, xend = Depth_end, y = Value, yend = Value_end, color = Facies), size = 4.5) +
   geom_point(aes(x = Depth, y = Value, color = Facies), size = 4) +
   scale_colour_paletteer_d("tvthemes::Bismuth") +
-  geom_vline(xintercept = well$Depth[est_cp[sapply(sets, function(set) min(apply(abs(outer(set, true_cp, `-`)),1,min))) > 1]],
+  geom_vline(xintercept = est_cp[sapply(sets, function(set) min(apply(abs(outer(set, true_cp, `-`)),1,min))) >= 1],
              linetype = "dashed", linewidth = 1.1, color = "black", alpha = 0.6) +
-  geom_vline(xintercept =  well$Depth[est_cp[sapply(sets, function(set) min(apply(abs(outer(set, true_cp, `-`)),1,min))) <= 1]],
-             linetype = "solid", linewidth = 1.25, color = "black", alpha = 0.6) +
+  geom_vline(xintercept =  est_cp[sapply(sets, function(set) min(apply(abs(outer(set, true_cp, `-`)),1,min))) < 1],
+             linetype = "solid", linewidth = 1.25, color = "red", alpha = 0.6) +
   facet_grid(rows = vars(Measurement), scales = "free_y") +
   theme_bw() +
   theme(strip.text = element_text(size = 20),
@@ -98,16 +104,23 @@ well_pivot %>%
         legend.title=element_text(size=20),
         legend.text=element_text(size=20)) +
   labs(title = "Lithology of Shankle Oil Well",
-       y=NULL, x = "Depth (ft)")
-
+       y=NULL, x = "Depth (ft)") + 
+  scale_x_continuous(breaks = seq(1, T, 40), labels = depth[seq(1, T, 40)])
 dev.off()
 
 # fit inspect ####
 inspect_fit <- inspect(t(as.matrix(well[,-c(1,2,8)])))
 est_cp = inspect_fit$changepoints[,1] + 1
+est_cp = est_cp[-c(1,length(est_cp))]
+
+abs(length(true_cp) - length(est_cp))
+fnsle(c(1,true_cp,T), c(1,est_cp,T))
+fpsle(c(1,true_cp,T), c(1,est_cp,T))
+
+sum(apply(abs(outer(est_cp, true_cp, `-`)), 2, min) < 1)
 sum(apply(abs(outer(est_cp, true_cp, `-`)), 2, min) <= 1)
 
-png("~/Desktop/inspect_well_log.png", width = 1300, height = (6.5 / 10) * 1300)
+png("./inspect_well_log.png", width = 1300, height = (6.5 / 10) * 1300)
 well_pivot %>%
   mutate(Value_end = well_pivot2$Value,
          Depth_end = well_pivot2$Depth) %>%
@@ -115,10 +128,10 @@ well_pivot %>%
   geom_segment(aes(x = Depth, xend = Depth_end, y = Value, yend = Value_end, color = Facies), size = 4.5) +
   geom_point(aes(x = Depth, y = Value, color = Facies), size = 4) +
   scale_colour_paletteer_d("tvthemes::Bismuth") +
-  geom_vline(xintercept = well$Depth[est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) > 1]],
+  geom_vline(xintercept = est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) >= 1],
              linetype = "dashed", linewidth = 1.1, color = "black", alpha = 0.6) +
-  geom_vline(xintercept =  well$Depth[est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) <= 1]],
-             linetype = "solid", linewidth = 1.25, color = "black", alpha = 0.6) +
+  geom_vline(xintercept = est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) < 1],
+             linetype = "solid", linewidth = 1.25, color = "red", alpha = 0.6) +
   facet_grid(rows = vars(Measurement), scales = "free_y") +
   theme_bw() +
   theme(strip.text = element_text(size = 20),
@@ -130,16 +143,22 @@ well_pivot %>%
         legend.title=element_text(size=20),
         legend.text=element_text(size=20)) +
   labs(title = "Lithology of Shankle Oil Well",
-       y=NULL, x = "Depth (ft)")
+       y=NULL, x = "Depth (ft)") + 
+  scale_x_continuous(breaks = seq(1, T, 40), labels = depth[seq(1, T, 40)])
 dev.off()
 
 # fit l2hdchange ####
 ts_l2_fit <- ts_hdchange(t(as.matrix(well[,-c(1,2)])), window_size = 5, N_rep = 1e5)
 l2_fit <- hdchange(ts_l2_fit)
 est_cp = l2_fit$time_stamps + 1
-sum(apply(abs(outer(est_cp, true_cp, `-`)), 1, min) <= 1)
 
-png("~/l2hdc_well_log.png", width = 1300, height = (6.5 / 10) * 1300)
+abs(length(true_cp) - length(est_cp))
+fnsle(c(1,true_cp,T), c(1,est_cp,T))
+fpsle(c(1,true_cp,T), c(1,est_cp,T))
+sum(apply(abs(outer(est_cp, true_cp, `-`)), 2, min) < 1)
+sum(apply(abs(outer(est_cp, true_cp, `-`)), 2, min) <= 1)
+
+png("./l2hdc_well_log.png", width = 1300, height = (6.5 / 10) * 1300)
 well_pivot %>%
   mutate(Value_end = well_pivot2$Value,
          Depth_end = well_pivot2$Depth) %>%
@@ -147,12 +166,10 @@ well_pivot %>%
   geom_segment(aes(x = Depth, xend = Depth_end, y = Value, yend = Value_end, color = Facies), size = 4.5) +
   geom_point(aes(x = Depth, y = Value, color = Facies), size = 4) +
   scale_colour_paletteer_d("tvthemes::Bismuth") +
-  geom_vline(xintercept = 2852.5,
-             linetype = "dashed", linewidth = 1.1, color = "red", alpha = 0.6) +
-  geom_vline(xintercept = well$Depth[est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) > 1]],
+  geom_vline(xintercept = est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) >= 1],
              linetype = "dashed", linewidth = 1.1, color = "black", alpha = 0.6) +
-  geom_vline(xintercept =  well$Depth[est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) <= 1]],
-             linetype = "solid", linewidth = 1.25, color = "black", alpha = 0.6) +
+  geom_vline(xintercept = est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) < 1],
+             linetype = "solid", linewidth = 1.25, color = "red", alpha = 0.6) +
   facet_grid(rows = vars(Measurement), scales = "free_y") +
   theme_bw() +
   theme(strip.text = element_text(size = 20),
@@ -164,17 +181,23 @@ well_pivot %>%
         legend.title=element_text(size=20),
         legend.text=element_text(size=20)) +
   labs(title = "Lithology of Shankle Oil Well",
-       y=NULL, x = "Depth (ft)")
+       y=NULL, x = "Depth (ft)") + 
+  scale_x_continuous(breaks = seq(1, T, 40), labels = depth[seq(1, T, 40)])
 dev.off()
 
 # fit ecp ####
-inspect_fit <- e.divisive(as.matrix(well[,-c(1,2)]), sig.lvl = 0.01,
+ecp_fit <- e.divisive(as.matrix(well[,-c(1,2)]), sig.lvl = 0.01,
                           min.size = 2, alpha = 2,
                           R=499)
-est_cp <- inspect_fit$estimates[-c(1, length(inspect_fit$estimates))]
-#sum(apply(abs(outer(est_cp, true_cp, `-`)), 2, min) <= 1)
+est_cp <- ecp_fit$estimates[-c(1, length(ecp_fit$estimates))]
 
-png("~/ecp_well_log.png", width = 1300, height = (6.5 / 10) * 1300)
+abs(length(true_cp) - length(est_cp))
+fnsle(c(1,true_cp,T), c(1,est_cp,T))
+fpsle(c(1,true_cp,T), c(1,est_cp,T))
+sum(apply(abs(outer(est_cp, true_cp, `-`)), 2, min) < 1)
+sum(apply(abs(outer(est_cp, true_cp, `-`)), 2, min) <= 1)
+
+png("./ecp_well_log.png", width = 1300, height = (6.5 / 10) * 1300)
 well_pivot %>%
   mutate(Value_end = well_pivot2$Value,
          Depth_end = well_pivot2$Depth) %>%
@@ -182,10 +205,10 @@ well_pivot %>%
   geom_segment(aes(x = Depth, xend = Depth_end, y = Value, yend = Value_end, color = Facies), size = 4.5) +
   geom_point(aes(x = Depth, y = Value, color = Facies), size = 4) +
   scale_colour_paletteer_d("tvthemes::Bismuth") +
-  geom_vline(xintercept = well$Depth[est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) > 1]],
+  geom_vline(xintercept = est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) >= 1],
              linetype = "dashed", linewidth = 1.1, color = "black", alpha = 0.6) +
-  geom_vline(xintercept =  well$Depth[est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) <= 1]],
-             linetype = "solid", linewidth = 1.25, color = "black", alpha = 0.6) +
+  geom_vline(xintercept =  est_cp[apply(abs(outer(est_cp, true_cp, `-`)),1,min) < 1],
+             linetype = "solid", linewidth = 1.25, color = "red", alpha = 0.6) +
   facet_grid(rows = vars(Measurement), scales = "free_y") +
   theme_bw() +
   theme(strip.text = element_text(size = 20),
@@ -197,6 +220,7 @@ well_pivot %>%
         legend.title=element_text(size=20),
         legend.text=element_text(size=20)) +
   labs(title = "Lithology of Shankle Oil Well",
-       y=NULL, x = "Depth (ft)")
+       y=NULL, x = "Depth (ft)") + 
+  scale_x_continuous(breaks = seq(1, T, 40), labels = depth[seq(1, T, 40)])
 dev.off()
 
